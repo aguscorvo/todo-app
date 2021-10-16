450d434ce416affc728be2626e0b1c37671920f9
aguscorvo <aguscorvo@gmail.com>
2021-10-16 16:36:26 -0300

    Add components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import { TodoCounter } from "./TodoCounter"
+import { TodoSearch } from "./TodoSearch"
+import { TodoList } from "./TodoList"
+import { TodoItem } from "./TodoItem"
+import { CreateTodoButton } from "./CreateTodoButton"
+// import "./App.css"
 
-function App() {
+const todos = [
+  { text: "Limpiar el cuarto", completed: false },
+  { text: "Tomar el curso de intro a React", completed: false },
+  { text: "Hacer la entrega de testing", completed: false },
+]
+
+function App(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <React.Fragment>
+      <TodoCounter />
+      <TodoSearch />
+      <TodoList>
+        {todos.map(todo => (
+          <TodoItem key={todo.text} text={todo.text} />
+        ))}
+      </TodoList>
+      <CreateTodoButton />
+    </React.Fragment>
+  )
 }
 
-export default App;
+export default App
