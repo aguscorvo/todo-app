9c3689684808eb09d8e7c64d7c02c984671ab2e5
aguscorvo <aguscorvo@gmail.com>
2021-10-21 19:35:57 -0300

    Add complete and delete todo features

--- src/App.js
+++ src/App.js
@@ -31,6 +31,20 @@ function App() {
     })
   }
 
+  const completeTodo = text => {
+    const todoIndex = todos.findIndex(todo => todo.text === text)
+    const newTodos = [...todos]
+    newTodos[todoIndex].completed = true
+    setTodos(newTodos)
+  }
+
+  const deleteTodo = text => {
+    const todoIndex = todos.findIndex(todo => todo.text === text)
+    const newTodos = [...todos]
+    newTodos.splice(todoIndex, 1)
+    setTodos(newTodos)
+  }
+
   return (
     <React.Fragment>
       <TodoCounter total={totalTodos} completed={completedTodos} />
@@ -41,6 +55,8 @@ function App() {
             key={todo.text}
             text={todo.text}
             completed={todo.completed}
+            onComplete={() => completeTodo(todo.text)}
+            onDelete={() => deleteTodo(todo.text)}
           />
         ))}
       </TodoList>
--- src/TodoItem.js
+++ src/TodoItem.js
@@ -2,20 +2,17 @@ import React from "react"
 import "./TodoItem.css"
 
 function TodoItem(props) {
-  const onComplete = () => alert(`Ya completaste el todo ${props.text}`)
-  const onDelete = () => alert(`Borraste el todo ${props.text}`)
-
   return (
     <li className='TodoItem'>
       <span
         className={`Icon Icon-check ${props.completed && "Icon-check--active"}`}
-        onClick={onComplete}>
+        onClick={props.onComplete}>
         √
       </span>
       <p className={`TodoItem-p ${props.completed && "TodoItem-p--complete"}`}>
         {props.text}
       </p>
-      <span className='Icon Icon-delete' onClick={onDelete}>
+      <span className='Icon Icon-delete' onClick={props.onDelete}>
         X
       </span>
     </li>
